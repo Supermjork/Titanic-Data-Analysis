9c128260c2e6b84f2485812cae1807a747bb72de
Supermjork <markehabaziz@gmail.com>
2022-12-02 10:53:20 +0200

    Added dirs, imported csv, loaded said csv into dataframe

---
+++ iceberg-analysis.R
@@ -0,0 +1,7 @@
+# READ: plots should be saved in "plots/", with descriptive names
+#       and optionally, a higher resolution, to be used in report
+#       might also help to "beautify" the plots into more eye-pleasing
+#       graphs, without compromising anything of importance
+
+# Loading the csv with passenger data into "titanic" data frame
+titanic <- read.csv("dataset/train.csv")
