ba4db15f2586d264cfe5a43c71e84d4b05df427c
Supermjork <markehabaziz@gmail.com>
2022-12-03 17:20:25 +0200

    Pls use na.omit, na.rm = TRUE is aids

--- iceberg-analysis.R
+++ iceberg-analysis.R
@@ -15,24 +15,26 @@ titanic <- read.csv("dataset/train.csv")
 # Answer to Q1, using summarize function
   # Trying to summarise multiple columns
     # Putting labels, functions into vectors and list for encapsulation
+    # (Would've been better to just use na.omit(titanic) tbh but it's in Q4)
 summarised_columns <- c("Age", "Fare")
-summary_functions <- list(min = min,
-                          max = max,
-                          q1 =~ quantile(., 0.25),
-                          q3 =~ quantile(., 0.75),
-                          mean = mean,
-                          sigma = sd)
+summary_functions <- list(min =~ min(., na.rm = TRUE),
+                          max =~ max(., na.rm = TRUE),
+                          q1 =~ quantile(., 0.25, na.rm = TRUE),
+                          q3 =~ quantile(., 0.75, na.rm = TRUE),
+                          mean =~ mean(., na.rm = TRUE),
+                          sigma =~ sd(., na.rm = TRUE),
+                          iqr =~ IQR(., na.rm = TRUE))
+
 summary_matrix_row <- c("Minimum Value",
                         "Maximum Value", 
                         "First Quartile", 
                         "Third Quartile", 
                         "Mean Population",
-                        "Sigma Population")
+                        "Sigma Population",
+                        "Inter Quantile Range")
 
     # Putting summarised data into variable
-titanic_summarise <- titanic %>% summarise_at(summarised_columns,
-                                                    summary_functions,
-                                                    na.rm = TRUE)
+titanic_summarise <- titanic %>% summarise_at(summarised_columns, summary_functions)
 titanic_summarise
   
     # Printing summarised data in a neat manner instead of 1 continuous row
@@ -43,24 +45,24 @@ matrix(titanic_summarise, nrow = length(summary_functions),
 
   # Alt method: Summarising each column individually
     # First summary regards Age
-titanic %>% summarise(age_min = min(Age),
-                            age_max = max(Age),
-                            age_q1 = quantile(Age, 0.25),
-                            age_q3 = quantile(Age, 0.75),
-                            age_med = median(Age),
-                            age_mu = mean(Age),
-                            age_sigma = sd(Age),
-                            age_iqr = IQR(Age))
+titanic %>% summarise(age_min = min(Age, na.rm = TRUE),
+                            age_max = max(Age, na.rm = TRUE),
+                            age_q1 = quantile(Age, 0.25, na.rm = TRUE),
+                            age_q3 = quantile(Age, 0.75, na.rm = TRUE),
+                            age_med = median(Age, na.rm = TRUE),
+                            age_mu = mean(Age, na.rm = TRUE),
+                            age_sigma = sd(Age, na.rm = TRUE),
+                            age_iqr = IQR(Age, na.rm = TRUE))
 
   # Second summary regards Fare
-titanic %>% summarize(fare_min = min(Fare),
-                            fare_max = max(Fare),
-                            fare_q1 = quantile(Fare, 0.25),
-                            fare_q3 = quantile(Fare, 0.75),
-                            fare_med = median(Fare),
-                            fare_mu = mean(Fare),
-                            fare_sigma = sd(Fare),
-                            fare_iqr = IQR(Fare))
+titanic %>% summarize(fare_min = min(Fare, na.rm = TRUE),
+                            fare_max = max(Fare, na.rm = TRUE),
+                            fare_q1 = quantile(Fare, 0.25, na.rm = TRUE),
+                            fare_q3 = quantile(Fare, 0.75, na.rm = TRUE),
+                            fare_med = median(Fare, na.rm = TRUE),
+                            fare_mu = mean(Fare, na.rm = TRUE),
+                            fare_sigma = sd(Fare, na.rm = TRUE),
+                            fare_iqr = IQR(Fare, na.rm = TRUE))
 
 # Cleaned dataset (no NANs, Will be working with this)
 titanic_clean = na.omit(titanic)
