21b2c921d6d3b02fc155925b8bcea6828c6ed543
LinlyBoi <libkyy@e.email>
2022-12-03 20:21:26 +0200

    Reformatted the main file (see comment for more)

--- iceberg-analysis.R
+++ iceberg-analysis.R
@@ -17,31 +17,32 @@ titanic <- read.csv("dataset/train.csv")
     # Putting labels, functions into vectors and list for encapsulation
     # (Would've been better to just use na.omit(titanic) tbh but it's in Q4)
 summarised_columns <- c("Age", "Fare")
-summary_functions <- list(min =~ min(., na.rm = TRUE),
-                          max =~ max(., na.rm = TRUE),
-                          q1 =~ quantile(., 0.25, na.rm = TRUE),
-                          q3 =~ quantile(., 0.75, na.rm = TRUE),
-                          mean =~ mean(., na.rm = TRUE),
-                          sigma =~ sd(., na.rm = TRUE),
-                          iqr =~ IQR(., na.rm = TRUE))
+summary_functions <- list(min = ~ min(., na.rm = TRUE),
+                          max = ~ max(., na.rm = TRUE),
+                          q1 = ~ quantile(., 0.25, na.rm = TRUE),
+                          q3 = ~ quantile(., 0.75, na.rm = TRUE),
+                          mean = ~ mean(., na.rm = TRUE),
+                          sigma = ~ sd(., na.rm = TRUE),
+                          iqr = ~ IQR(., na.rm = TRUE))
 
 summary_matrix_row <- c("Minimum Value",
-                        "Maximum Value", 
-                        "First Quartile", 
-                        "Third Quartile", 
+                        "Maximum Value",
+                        "First Quartile",
+                        "Third Quartile",
                         "Mean Population",
                         "Sigma Population",
                         "Inter Quantile Range")
 
     # Putting summarised data into variable
-titanic_summarise <- titanic %>% summarise_at(summarised_columns, summary_functions)
+titanic_summarise <- titanic %>% summarise_at(summarised_columns,
+  summary_functions)
 #titanic_summarise
-  
     # Printing summarised data in a neat manner instead of 1 continuous row
 matrix(titanic_summarise, nrow = length(summary_functions),
        ncol = length(summarised_columns),
        dimnames = list(summary_matrix_row, summarised_columns),
        byrow = TRUE)
 
-# Cleaned dataset (no NANs, Will be working with this.)
-titanic_clean = na.omit(titanic)
+# Cleaned dataset (no NANs, Will be working with this.) 4th Requirement done
+titanic_clean <- na.omit(titanic)
+hist(titanic$Age)
