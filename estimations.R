1359564a7f9cbf449200243fe9e41258f83019a3
LinlyBoi <libkyy@e.email>
2022-12-10 23:37:42 +0200

    i hid an easter egg in the code. can you find it?

--- estimations.R
+++ estimations.R
@@ -5,6 +5,3 @@ mme_estimator_bias <- function(population, sample_size, col_name) {
   estimate_value <- mean(sample_mean[[col_name]])
   return(estimate_value - true_value)
 }
-mme_age_sample50_bias <- mme_estimator_bias(population = titanic_clean,
-                                            sample_size = 50,
-                                            col_name = "Age")
