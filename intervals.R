a57c3c1cab70a9fba6d9c7042d0876ba7bfd6ff0
LinlyBoi <libkyy@e.email>
2022-12-16 21:23:13 +0200

    Spelling mistakes and debuggging (idot)

--- intervals.R
+++ intervals.R
@@ -9,9 +9,27 @@ conf_interval <- function(x_bar, s, n, confidence) {
     return(NULL)
   }
   # Get margin of error
-  margin <- qt(confidence, df = n - 1)
+  margin <- qt(confidence, df = n - 1) * s / sqrt(n)
   lowerinterval <- x_bar - margin
   upperinterval <- x_bar + margin
-  print(paste0("Inteveral is: [", lowerinterval, ",", upperinterval, "]"))
+  print(paste0("Interval is: [", lowerinterval, ",", upperinterval, "]"))
+  return(c(lowerinterval, upperinterval))
+}
+# Same as conf_interval but takes a vector for x_bar and s
+conf_interval2 <- function(vec, n, confidence = 0.95) {
+  # in case people REALLY don't like putting that .
+  if (confidence > 1) {
+    confidence <- confidence / 100
+  }else if (confidence <= 0) {
+    print("You are a horrible person >:(")
+    return(NULL)
+  }
+  x_bar <- vec[1]
+  s <- vec[2]
+  # Get margin of error
+  margin <- qt(confidence, df = n - 1) * s / sqrt(n)
+  lowerinterval <- x_bar - margin
+  upperinterval <- x_bar + margin
+  print(paste0("Interval is: [", lowerinterval, ",", upperinterval, "]"))
   return(c(lowerinterval, upperinterval))
 }
